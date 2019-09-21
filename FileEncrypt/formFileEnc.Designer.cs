8a609a9e30bd38d738fc70d7d6c396f1554770a5
zhxiao2012 <Peter Xiao>
2019-09-21 23:46:11 +1200

    File encrypt / decrypt with tripleDES

---
+++ FileEncrypt/formFileEnc.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace FileEncrypt
+{
+    partial class formFileEnc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Browse = new System.Windows.Forms.Button();
+            this.enCrypt = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textKey = new System.Windows.Forms.TextBox();
+            this.textFile = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Decrypt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Browse
+            // 
+            this.Browse.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Browse.Location = new System.Drawing.Point(434, 26);
+            this.Browse.Name = "Browse";
+            this.Browse.Size = new System.Drawing.Size(75, 23);
+            this.Browse.TabIndex = 1;
+            this.Browse.Text = "Browse";
+            this.Browse.UseVisualStyleBackColor = true;
+            this.Browse.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // enCrypt
+            // 
+            this.enCrypt.Location = new System.Drawing.Point(434, 57);
+            this.enCrypt.Name = "enCrypt";
+            this.enCrypt.Size = new System.Drawing.Size(75, 23);
+            this.enCrypt.TabIndex = 2;
+            this.enCrypt.Text = "EnCrypt";
+            this.enCrypt.UseVisualStyleBackColor = true;
+            this.enCrypt.Click += new System.EventHandler(this.enCrypt_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "KEY";
+            // 
+            // textKey
+            // 
+            this.textKey.Location = new System.Drawing.Point(78, 57);
+            this.textKey.Name = "textKey";
+            this.textKey.Size = new System.Drawing.Size(331, 21);
+            this.textKey.TabIndex = 4;
+            // 
+            // textFile
+            // 
+            this.textFile.Location = new System.Drawing.Point(78, 26);
+            this.textFile.Name = "textFile";
+            this.textFile.Size = new System.Drawing.Size(331, 21);
+            this.textFile.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "FILE";
+            // 
+            // Decrypt
+            // 
+            this.Decrypt.Location = new System.Drawing.Point(434, 86);
+            this.Decrypt.Name = "Decrypt";
+            this.Decrypt.Size = new System.Drawing.Size(75, 23);
+            this.Decrypt.TabIndex = 7;
+            this.Decrypt.Text = "DeCrypt";
+            this.Decrypt.UseVisualStyleBackColor = true;
+            this.Decrypt.Click += new System.EventHandler(this.Decrypt_Click);
+            // 
+            // formFileEnc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(554, 128);
+            this.Controls.Add(this.Decrypt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textFile);
+            this.Controls.Add(this.textKey);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.enCrypt);
+            this.Controls.Add(this.Browse);
+            this.Name = "formFileEnc";
+            this.Text = "File Encryptor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Browse;
+        private System.Windows.Forms.Button enCrypt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textKey;
+        private System.Windows.Forms.TextBox textFile;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Decrypt;
+    }
+}
+
---
+++ FileEncrypt/formFileEnc.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace FileEncrypt
+{
+    public partial class formFileEnc : Form
+    {
+
+        private OpenFileDialog openFileDialog1;
+        public formFileEnc()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1 = new OpenFileDialog();
+            // Display the openFile dialog.
+            DialogResult result = openFileDialog1.ShowDialog();
+
+            // OK button was pressed.
+            if (result == DialogResult.OK)
+            {
+                this.textFile.Text = openFileDialog1.FileName;
+
+                Invalidate();
+            }
+
+            // Cancel button was pressed.
+            else if (result == DialogResult.Cancel)
+            {
+                return;
+            }
+
+        }
+
+        struct tDes{
+            public string inName;
+            public string outName;
+            public byte[] desKey;
+            public byte[] desIV;
+        };
+
+      //  private static void EncryptFile(String inName, String outName, byte[] desKey, byte[] desIV)
+        private static void EncryptFile(object param)
+        {
+            tDes td = (tDes)param;
+            //Create the file streams to handle the input and output files.  
+            FileStream fin = new FileStream(td.inName, FileMode.Open, FileAccess.Read);
+            FileStream fout = new FileStream(td.outName, FileMode.OpenOrCreate, FileAccess.Write);
+            fout.SetLength(0);
+            //Create variables to help with read and write.  
+            byte[] bin = new byte[100]; //This is intermediate storage for the encryption.  
+            long rdlen = 0; //This is the total number of bytes written.  
+            long totlen = fin.Length; //This is the total length of the input file.  
+            int len; //This is the number of bytes to be written at a time.  
+            try
+            {
+                TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+                CryptoStream encStream = new CryptoStream(fout, tdes.CreateEncryptor(td.desKey, td.desIV), CryptoStreamMode.Write);
+
+
+                //Read from the input file, then encrypt and write to the output file.
+                while (rdlen < totlen)
+                {
+                    len = fin.Read(bin, 0, 100);
+                    encStream.Write(bin, 0, len);
+                    rdlen = rdlen + len;
+                }
+                encStream.Close();
+                fout.Close();
+                fin.Close();
+                MessageBox.Show("finshed.");
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.Message);
+                fout.Close();
+                fin.Close();
+                Thread.CurrentThread.Abort();
+            }
+        }
+        private static void DecryptFile(object param)
+        {
+            tDes td = (tDes)param;
+            //Create the file streams to handle the input and output files.  
+            FileStream fin = new FileStream(td.inName, FileMode.Open, FileAccess.Read);
+            FileStream fout = new FileStream(td.outName, FileMode.OpenOrCreate, FileAccess.Write);
+            fout.SetLength(0);
+            //Create variables to help with read and write.  
+            byte[] bin = new byte[100]; //This is intermediate storage for the encryption.  
+            long rdlen = 0; //This is the total number of bytes written.  
+            long totlen = fin.Length; //This is the total length of the input file.  
+            int len; //This is the number of bytes to be written at a time.  
+            try
+            {
+                TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+                CryptoStream decStream = new CryptoStream(fout, tdes.CreateDecryptor(td.desKey, td.desIV), CryptoStreamMode.Write);
+          
+        
+
+                //Read from the input file, then encrypt and write to the output file.
+                while (rdlen < totlen)
+                {
+                   len = fin.Read(bin, 0, 100);
+                   decStream.Write(bin, 0, len);
+                   rdlen = rdlen + len;
+                }
+                decStream.Close();
+                fout.Close();
+                fin.Close();
+                MessageBox.Show("finshed.");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                fout.Close();
+                fin.Close();
+                Thread.CurrentThread.Abort();
+            }
+        }
+        private void enCrypt_Click(object sender, EventArgs e)
+        {
+            //   byte[] key= Encoding.Unicode.GetBytes(textKey.Text);
+
+        
+            if (textKey.TextLength == 0 || textFile.TextLength == 0)
+            {
+                MessageBox.Show("Please select file and input a key.");
+
+            }
+            else
+            {
+                tDes td = new tDes();
+                td.desKey = new byte[24];
+                td.desIV = new byte[8];
+                StringBuilder sb = new StringBuilder("", 32);
+                do
+                {
+                    sb.Append(textKey.Text);
+
+                } while (sb.Length < 32);
+                Encoding.UTF8.GetBytes(sb.ToString(), 0, 24, td.desKey, 0);
+                Encoding.UTF8.GetBytes(sb.ToString(), 24, 8, td.desIV, 0);
+
+                td.inName = textFile.Text;
+                td.outName = textFile.Text + "_";
+                Thread th = new Thread(EncryptFile);
+                th.Start(td);
+               
+                               
+            }
+        }
+
+        private void Decrypt_Click(object sender, EventArgs e)
+        {
+            
+
+         
+            if (textKey.TextLength == 0 || textFile.TextLength == 0)
+            {
+                MessageBox.Show("Please select file and input a key.");
+
+            }
+            else
+            {
+                tDes td = new tDes();
+                td.desKey = new byte[24];
+                td.desIV = new byte[8];
+                StringBuilder sb = new StringBuilder("", 32);
+                do
+                {
+                    sb.Append(textKey.Text);
+
+                } while (sb.Length < 32);
+                Encoding.UTF8.GetBytes(sb.ToString(), 0, 24, td.desKey, 0);
+                Encoding.UTF8.GetBytes(sb.ToString(), 24, 8, td.desIV, 0);
+
+                td.inName = textFile.Text;
+                td.outName = textFile.Text + "_";
+                Thread th = new Thread(DecryptFile);
+                th.Start(td);
+                   
+                                  
+            }
+        }
+    }
+}
